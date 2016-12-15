3e817fbc118920ea640d250f646cbce83aee9892
Unknown <rodrigo.scheuer@cwi.com.br>
2016-12-15 15:43:35 -0200

    Feat(LoginController, viewLogin): implementar tela de login adm.

---
+++ InscricoesCrescer/InscricoesCrescer/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InscricoesCrescer.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult FazerLogin()
+        {
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
