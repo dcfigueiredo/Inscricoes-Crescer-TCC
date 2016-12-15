e9fe5d8d1f276de177f95853f61fd3bd7d08d43c
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-15 10:44:24 -0200

    feat(InscricoesCrescer/Web): Html para inscrição de novos candidatos com model

---
+++ InscricoesCrescer/InscricoesCrescer/Models/CandidatoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace InscricoesCrescer.Models
+{
+    public class CandidatoModel
+    {
+        public long? Id { get; set; }
+
+        [Required]
+        [DisplayName("Nome completo:")]
+        public string Nome { get; set; }
+
+        [Required]
+        [DisplayName("Nome completo:")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Instituição de ensino:")]
+        public string Instituicao { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Data de concluão do curso:")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Conclusao { get; set; }
+    }
+}
