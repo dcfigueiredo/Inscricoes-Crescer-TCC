d6f97c8dc83a5bed3fcab5974fb3ea8aa61f85d6
Anna Luísa <annaluisa1703@gmail.com>
2016-12-15 17:35:37 -0200

    feat(Web, Dominio): implementar model e entidade administrador.

---
+++ InscricoesCrescer/InscricoesCrescer.Dominio/LoginAdm/AdministradorEntidade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InscricoesCrescer.Dominio.Login_Adm
+{
+    public class AdministradorEntidade
+    {
+        public long? Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
---
+++ InscricoesCrescer/InscricoesCrescer/Models/AdministradorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InscricoesCrescer.Models
+{
+    public class AdministradorModel
+    {
+        [Required]
+        [DisplayName("E-mail:")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Senha:")]
+        public string Senha { get; set; }
+
+    }
+}
