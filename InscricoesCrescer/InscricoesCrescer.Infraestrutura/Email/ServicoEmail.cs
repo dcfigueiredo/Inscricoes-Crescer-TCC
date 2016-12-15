17820e349aa28df584797bd38911a80066012cc2
Unknown <rodrigo.scheuer@cwi.com.br>
2016-12-15 14:18:25 -0200

    Feat(ServicoDeEmail): enviar email para confirmação de cadastro.

---
+++ InscricoesCrescer/InscricoesCrescer.Infraestrutura/Email/ServicoEmail.cs
@@ -0,0 +1,47 @@
+﻿using InscricoesCrescer.Dominio.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InscricoesCrescer.Infraestrutura.Email
+{
+    public class ServicoEmail : IServicoEmail
+    {
+        public bool enviarEmailConfirmacao(string para, string linkConfirmacao)
+        {
+            // Hotmail                                          gmail
+            //smtp.Host = "smtp.live.com";                  smtp.Host = "smtp.gmail.com";
+            //smtp.Port = 587;                              smtp.Port = 465;
+            //email:rodrigo.scheuer@hotmail.com             email:  emailTesteCwi@gmail.com
+            //                                              senha: GAoXIP3tC0Qv
+            string assunto = "Confirmação de cadastro no projeto Crescer";
+            string mensagem = "Link de confirmação aqui -> ";
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            MailMessage mail = new MailMessage("emailTesteCwi@gmail.com", para, assunto, mensagem);
+
+            smtp.UseDefaultCredentials = false;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new System.Net.NetworkCredential("emailTesteCwi@gmail.com", "GAoXIP3tC0Qv");
+            //smtp.Timeout = 20000;
+
+            try
+            {
+                smtp.Send(mail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                mail.Dispose();
+            }
+        }
+    }
+}
--- InscricoesCrescer/InscricoesCrescer/Controllers/HomeController.cs
+++ InscricoesCrescer/InscricoesCrescer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Text.RegularExpressions;
+using System.Web.Mvc;
 
 namespace InscricoesCrescer.Controllers
 {
@@ -8,6 +9,20 @@ namespace InscricoesCrescer.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public static bool ValidaEnderecoEmail(string enderecoEmail)
+        {
+            Regex expressaoRegex = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
+
+            if (expressaoRegex.IsMatch(enderecoEmail))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
