1a9e05e827c78488e3fc8f1dfb8f8a93479df528
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-15 18:00:29 -0200

    refactor(Repositorio): Mudar o nome da propriedade do candidato no contexto de dados

---
+++ InscricoesCrescer/InscricoesCrescer.Repositorio/Migrations/201612151941577_MudarNomeDaPropriedadeCandidatoEntidade.cs
@@ -0,0 +1,18 @@
+namespace InscricoesCrescer.Repositorio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MudarNomeDaPropriedadeCandidatoEntidade : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CandidatoEntidade", "Curso", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CandidatoEntidade", "Curso");
+        }
+    }
+}
