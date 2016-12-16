f097f0e1ad11116848a0c947dbc3ec68129bd188
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-16 16:56:06 -0200

    feat(Administrador): Adicionar sql que inserie administradores no banco.

---
+++ InserirAdministradores.sql
@@ -0,0 +1,8 @@
+insert into AdministradorEntidade (Email, Senha)
+values('cesarauguso@cwi.com.br', '827ccb0eea8a706c4c34a16891f84e7b')
+
+insert into AdministradorEntidade (Email, Senha)
+values('carol.leite@cwi.com.br','827ccb0eea8a706c4c34a16891f84e7b')
+
+insert into AdministradorEntidade (Email, Senha)
+values ('andre.nunes@cwi.com.br','827ccb0eea8a706c4c34a16891f84e7b')
