d06aa710737fe8e621a7a4d3434588255bec2c48
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-17 20:03:52 -0200

    refactor(AdministrativoController): Remover métodos que não são chamados em lugar algum

--- InscricoesCrescer/InscricoesCrescer/Controllers/AdministrativoController.cs
+++ InscricoesCrescer/InscricoesCrescer/Controllers/AdministrativoController.cs
@@ -23,7 +23,6 @@ namespace InscricoesCrescer.Controllers
         {            
             return View();
         }
-<<<<<<<
 
         public PartialViewResult CarregarListaDeCandidatos(int pagina)
         {
@@ -42,51 +41,5 @@ namespace InscricoesCrescer.Controllers
             model.QuantidadeDeItensPorPagina = servicoConfiguracao.QuantidadeDeCandidatosPorPagina;
             return model;
         }
-=======
-
-
-        private List<CandidatoModel2> convertListaParaCandidatoModel2(List<CandidatoEntidade> lista)
-        {
-            foreach (CandidatoEntidade item in lista)
-            {
-                CandidatoModel2 candidato = new CandidatoModel2();
-                candidato.Id = item.Id;
-                candidato.Nome = item.Nome;
-                DateTime dataNascimento = Convert.ToDateTime(item.DataNascimento);
-                candidato.Idade = CalculaIdade(dataNascimento);
-                candidato.Email = item.Email;
-                candidato.Telefone = item.Telefone;
-                candidato.Cidade = item.Cidade;
-                candidato.Instituicao = item.Instituicao;
-                candidato.Curso = item.Curso;
-                candidato.Conclusao = item.Conclusao;
-                candidato.Linkedin = item.Linkedin;
-                candidato.Status = item.Status;
-                candidato.Senha = item.Senha;
-            }
-            return null;
-        }
-
-        private static int CalculaIdade(DateTime dataNascimento)
-        {
-            if (dataNascimento != null)
-            {
-                // Retorna o número de anos
-                int idade = (DateTime.Now.Year - dataNascimento.Year);
-
-                // Se a data de aniversário não ocorreu ainda este ano, subtrair um ano a partir da idade
-                if (DateTime.Now.Month < dataNascimento.Month || (DateTime.Now.Month == dataNascimento.Month && DateTime.Now.Day < dataNascimento.Day))
-                {
-                    idade --;
-                }
-                return idade;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
->>>>>>>
     }
 }
