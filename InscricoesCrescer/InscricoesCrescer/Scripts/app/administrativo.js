d893fa0e2df500683aac7ffbec415885b984f9f2
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-17 21:44:05 -0200

    feat(IndexAdministrativo): listagem de candidatos por ajax (sem paginação).

--- InscricoesCrescer/InscricoesCrescer/Scripts/app/administrativo.js
+++ InscricoesCrescer/InscricoesCrescer/Scripts/app/administrativo.js
@@ -1,18 +1,23 @@
 ﻿var paginaAtual = 0;
-
+$('#listar-candidatos').on('click', carregarListaDeCandidatos(paginaAtual));
 $(function iniciarPaginaAdministrativo() {
-    $('#listar-candidatos').click(carregarListaDeCandidatos());
+    //executa para carregar a partial view padrão da index
+    carregarListaDeCandidatos(paginaAtual);
+
+    //listener pra recarregar a lista quando for clicado
+    
 });
 
-$(function carregarListaDeCandidatos(paginaAtual) {
+function carregarListaDeCandidatos(pagina) {
+    console.log("asdsadadd");
     $.ajax({
         url: 'Administrativo/CarregarListaDeCandidatos',
         type: 'GET',
-        data: { pagina: paginaAtual }
+        data: { pagina: pagina }
     })
     .then(function (partialView) {
         $('#container-partial-views').html(partialView);
 
     });
-});
+};
 
