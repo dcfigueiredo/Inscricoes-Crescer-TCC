451f0ef95e4c70e06c78edfb3874182715a01f83
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-17 20:29:59 -0200

    fix(QueryAdmins): Nome do cesar estava errado.

--- InserirAdministradores.sql
+++ InserirAdministradores.sql
@@ -1,8 +1,12 @@
+truncate table AdministradorEntidade
+
 insert into AdministradorEntidade (Email, Senha)
-values('cesarauguso@cwi.com.br', '827ccb0eea8a706c4c34a16891f84e7b')
+values('cesaraugusto@cwi.com.br', '827ccb0eea8a706c4c34a16891f84e7b')
 
 insert into AdministradorEntidade (Email, Senha)
 values('carol.leite@cwi.com.br','827ccb0eea8a706c4c34a16891f84e7b')
 
 insert into AdministradorEntidade (Email, Senha)
 values ('andre.nunes@cwi.com.br','827ccb0eea8a706c4c34a16891f84e7b')
+
+select * from AdministradorEntidade
