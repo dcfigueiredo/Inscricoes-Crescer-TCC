6e56b3ea2a31fb8b75bc373bf1c7e020adabe744
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-12-18 01:30:05 -0200

    feat(_PorocessoSeletivo , PorocessoSeletivo ViewModel): adicionar atributos à model e ajustar campos na view. Pendente.

---
+++ InscricoesCrescer/InscricoesCrescer/Models/ProcessoSeletivoViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InscricoesCrescer.Models
+{
+
+    //criar e importar servico
+    public class ProcessoSeletivoViewModel
+    {
+        public long? Id { get; set; }
+
+        public DateTime DataInicioEntrevistas { get; set; }
+        public DateTime DataSelecaoFinal { get; set; }
+        public DateTime DataInicioAulas { get; set; }
+        public DateTime DataFinalAulas { get; set; }
+        public DateTime AnoEdicao { get; set; }
+        public DateTime SemestreEdicao { get; set; }
+
+
+
+
+
+     private int verificarDataEdicao()
+     {
+
+        //TO DO:
+        //fazer um servico que busque estes dados para mim do banco!
+
+         if (semestreDoRegistroAnterior == 1)
+         {
+             anoDoNovoRegistro = anodoRegistroAnterior
+     
+             semestreDoNovoRegistro = semestreDoRegistroAnterior + 1
+         }
+         else
+         {
+             anoDoNovoRegistro = anoDoRegistroAnterior + 1
+     
+     
+             semestreDoNovoRegistro = 1;
+         }
+
+         
+
+    }
+}
