20cd6a15541c91a5c449961ddc8e1463589c5886
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-20 21:36:54 -0200

    test(EntrevistaServico): Adicionar testes para o servico de entrevista.

---
+++ InscricoesCrescer/InscricoesCrescer.Mock/Entrevista/EntrevistaRepositorioMock.cs
@@ -0,0 +1,79 @@
+﻿using InscricoesCrescer.Dominio.Candidato;
+using InscricoesCrescer.Dominio.Entrevista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InscricoesCrescer.Mock.Entrevista
+{
+    public class EntrevistaRepositorioMock : IEntrevistaRepositorio
+    {
+
+        public IList<EntrevistaEntidade> entrevistas = new List<EntrevistaEntidade>()
+        {
+            new EntrevistaEntidade ()
+            {
+                Id = 1,
+                CandidatoEntidadeId = 1,
+                Candidato = new CandidatoEntidade ()
+                {
+                    Id = 1,
+                    Nome = "Daniel de Carvalho Figueiredo",
+                },
+                DataEntrevista = new DateTime(2018, 12, 1),
+                EmailAdministrador = "carol.leite@cwi.com.br",
+                ParecerRH = "Parecer do RH",
+                ParecerTecnico = "Parecer Tecnico",
+                ProvaAC = 5,
+                ProvaG36 = 6,
+                ProvaTecnica = 10,                
+            },
+
+            new EntrevistaEntidade ()
+            {
+                Id = 2,
+                CandidatoEntidadeId = 1,
+                Candidato = new CandidatoEntidade ()
+                {
+                    Id = 1,
+                    Nome = "Daniel de Carvalho Figueiredo",
+                },
+                DataEntrevista = new DateTime(2017, 12, 1),
+                EmailAdministrador = "carol.leite@cwi.com.br",
+                ParecerRH = "Parecer do RH Com String louca caso de treta nos testes",
+                ParecerTecnico = "Parecer Tecnico",
+                ProvaAC = 5,
+                ProvaG36 = 10,
+                ProvaTecnica = 6,
+            }
+        };
+
+        public EntrevistaEntidade BuscarPorId(long id)
+        {
+            return this.entrevistas.FirstOrDefault(_ => _.Id == id);
+        }
+
+        public IList<EntrevistaEntidade> BuscarPorIdDoCandidato(long id)
+        {
+            return this.entrevistas.Where(_ => _.CandidatoEntidadeId == id).ToList();
+        }
+
+        public List<EntrevistaEntidade> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Criar(EntrevistaEntidade entrevista)
+        {
+            this.entrevistas.Add(entrevista);
+        }
+
+        public void Editar(EntrevistaEntidade entrevista)
+        {
+            int posicao = entrevistas.IndexOf(entrevista);
+            entrevistas.Insert(posicao, entrevista);
+        }
+    }
+}
---
+++ InscricoesCrescer/InscricoesCrescer.Test/EntrevistaServicoTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InscricoesCrescer.Dominio.Entrevista;
+using InscricoesCrescer.Mock.Entrevista;
+using System.Collections.Generic;
+using InscricoesCrescer.Dominio.Candidato;
+
+namespace InscricoesCrescer.Test
+{
+    [TestClass]
+    public class EntrevistaServicoTest
+    {
+        [TestMethod]
+        public void TestaBuscarEntrevistaComIdExistente()
+        {
+            EntrevistaRepositorioMock erm = new EntrevistaRepositorioMock();
+            EntrevistaServico servico = new EntrevistaServico(erm);
+
+            EntrevistaEntidade entrevistaEsperada = servico.BuscarPorId(1);
+
+            Assert.AreEqual(erm.entrevistas[0], entrevistaEsperada);
+        }
+
+        [TestMethod]
+        public void TestaBuscarEntrevistaComIdInexistente()
+        {
+            EntrevistaRepositorioMock erm = new EntrevistaRepositorioMock();
+            EntrevistaServico servico = new EntrevistaServico(erm);
+
+            EntrevistaEntidade entrevistaEsperada = servico.BuscarPorId(4);
+
+            Assert.IsNull(entrevistaEsperada);
+        }
+
+        [TestMethod]
+        public void TestaBuscarEntrevistasPeloIdDoCandidatoDeveRetornarDuas()
+        {
+            EntrevistaRepositorioMock erm = new EntrevistaRepositorioMock();
+            EntrevistaServico servico = new EntrevistaServico(erm);
+
+            IList<EntrevistaEntidade> entrevistasEsperadas = servico.BuscarPorIdDoCandidato(1);
+
+            Assert.AreEqual(2, entrevistasEsperadas.Count);
+        }
+
+        [TestMethod]
+        public void TestaBuscarEntrevistasPeloIdDoCandidatoQueNaoExisteDeveTerTamanhoZero()
+        {
+            EntrevistaRepositorioMock erm = new EntrevistaRepositorioMock();
+            EntrevistaServico servico = new EntrevistaServico(erm);
+
+            IList<EntrevistaEntidade> entrevistasEsperadas = servico.BuscarPorIdDoCandidato(3);
+
+            Assert.AreEqual(0, entrevistasEsperadas.Count);
+        }
+
+        [TestMethod]
+        public void TestaEditarUmaEntrevista()
+        {
+            EntrevistaRepositorioMock erm = new EntrevistaRepositorioMock();
+            EntrevistaServico servico = new EntrevistaServico(erm);
+            EntrevistaEntidade entrevistaEditada = erm.entrevistas[0];
+
+            entrevistaEditada.EmailAdministrador = "batatinhaquandonasce@cwi.com.br";
+
+            servico.Salvar(entrevistaEditada);
+
+            Assert.AreEqual("batatinhaquandonasce@cwi.com.br", erm.entrevistas[0].EmailAdministrador);
+        }
+
+        [TestMethod]
+        public void TestaCriarUmaEntrevista()
+        {
+            EntrevistaRepositorioMock erm = new EntrevistaRepositorioMock();
+            EntrevistaServico servico = new EntrevistaServico(erm);
+            EntrevistaEntidade entrevistaNova = new EntrevistaEntidade()
+            {
+                Id = 0,
+                CandidatoEntidadeId = 2,
+                Candidato = new CandidatoEntidade()
+                {
+                    Id = 2,
+                    Nome = "Goku da Silva Sauro",
+                },
+                DataEntrevista = new DateTime(2017, 12, 1),
+                EmailAdministrador = "carol.leite@cwi.com.br",
+                ParecerRH = "Parecer do RH Com String louca caso de treta nos testes de novo pq o bernardo uma vez falou que a gente deveria escrever testes inusitados, já que uma vez uma criptografia foi quebrada pq a senha começava com 0",
+                ParecerTecnico = "Parecer Tecnico",
+                ProvaAC = 5,
+                ProvaG36 = 10,
+                ProvaTecnica = 6,
+            };            
+
+            servico.Salvar(entrevistaNova);
+
+            Assert.AreEqual(3, erm.entrevistas.Count);
+        }
+    }
+}
