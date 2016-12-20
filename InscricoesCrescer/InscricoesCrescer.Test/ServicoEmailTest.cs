f2484378c9c2ca2d4ffa8de2fd4e0c68457a01f4
Rodrigo <rodrigo.scheuer@cwi.com.br>
2016-12-20 12:07:55 -0200

    feat(ServicoEmailTest): mais testes

--- InscricoesCrescer/InscricoesCrescer.Test/ServicoEmailTest.cs
+++ InscricoesCrescer/InscricoesCrescer.Test/ServicoEmailTest.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using InscricoesCrescer.Infraestrutura;
+using System.Collections.Generic;
+using InscricoesCrescer.Dominio.Candidato;
+using InscricoesCrescer.Servico;
+using InscricoesCrescer.Mock.Candidato;
 
 namespace InscricoesCrescer.Test
 {
@@ -25,6 +29,33 @@ namespace InscricoesCrescer.Test
             bool ehValido = servicoEmail.ValidaEmail(emailValido);
 
             Assert.AreEqual(false, ehValido);
+        }
+
+        [TestMethod]
+        public void TestarEnvioDeEmailDeConfirmacao()
+        {
+            bool confirmacao = servicoEmail.enviarEmailConfirmacao("rodrigo.scheuer@hotmail.com");
+            Assert.AreEqual(true, confirmacao);
+
+        }
+
+        [TestMethod]
+        public void TestarEnvioDeEmailDeNotificacao()
+        {
+            CandidatoServico candidatoServico = new CandidatoServico(new CandidatoRepositorioMock(), new ServicoConfiguracao());
+            List<CandidatoEntidade> lista = new List<CandidatoEntidade>();
+            lista = candidatoServico.BuscarInteressados();
+            DateTime dataInicio = Convert.ToDateTime("12/12/1992");
+            DateTime dataFim = Convert.ToDateTime("13/12/1992");
+            bool confirmacao = servicoEmail.enviarEmailDeNotificacao(lista, dataInicio, dataFim);
+            Assert.AreEqual(true, confirmacao);
+        }
+
+        [TestMethod]
+        public void TestarBuscaConfig()
+        {
+            string config = ServicoEmail.buscarConfiguracao("configuracao");
+            Assert.AreEqual("campo vazio.", config);
         }
     }
 }
