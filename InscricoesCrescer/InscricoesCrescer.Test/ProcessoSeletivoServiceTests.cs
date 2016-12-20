62a41833efcc68002514642794f0820ff1e1c8a3
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-12-20 02:36:42 -0200

    feat(ProcessoSeletivoServiceTests): implementar classe de testes unitarios para o método BuscarTodos.

---
+++ InscricoesCrescer/InscricoesCrescer.Test/ProcessoSeletivoServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InscricoesCrescer.Dominio.ProcessoSeletivo;
+using InscricoesCrescer.Mock.ProcessoSeletivo;
+
+namespace InscricoesCrescer.Test
+{
+    [TestClass]
+        public class ProcessoSeletivoServicoTest
+        {
+            [TestMethod]
+            public void TestaBuscarTodosOsProcessosCadastrados()
+            {
+                ProcessoSeletivoServico processoServico = new ProcessoSeletivoServico(new ProcessoSeletivoRepositorioMock());
+                IList<ProcessoSeletivoEntidade> processos = processoServico.BuscarTodos();
+                Assert.AreEqual(2, processos.Count);
+            }
+
+            
+        }
+    }
+
+
